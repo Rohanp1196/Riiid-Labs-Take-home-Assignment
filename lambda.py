78ce0d6a3b57a7e548183cdc282a173944d6ad83
Rohanp1196 <54926705+Rohanp1196@users.noreply.github.com>
2020-10-21 15:02:56 -0700

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -4,9 +4,7 @@ import urllib.request
 
 def lambda_handler(event, context):
     ''' Demonstrates a simple HTTP request from Lambda '''
-    #item_response = urllib.request.urlopen(base_url)
-    #response_string = item_response.read().decode('utf-8')
-    #item = json.loads(response_string)
+    
     base_url = 'https://hacker-news.firebaseio.com/v0/item/'
     answer = []
     for itemid in range(1, 10):
@@ -18,9 +16,4 @@ def lambda_handler(event, context):
         ts = int(time)
         ans = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         answer.append(ans)
-    #now = datetime.now()
-    #current_time = now.strftime("'%Y-%m-%d %H:%M:%S'")
-    #time = item['time']
-    #ts = int(time)
-    #ans = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     return answer
